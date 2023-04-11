670f227017e2d79e19b8125a5a40080d1e2938a2
Kim Yu kyeong <kyk940608@gmail.com>
2023-04-12 00:45:22 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.21 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        String replace = "";
+        replace = my_string.toLowerCase().replaceAll("[abcdefghijklmnopqrstuvwxyz]", "");
+        String[] arr = replace.split("");
+        
+        for(int i = 0; i < arr.length; i++) {
+            answer += Integer.parseInt(arr[i]);
+        }
+        
+        return answer;
+    }
+}
