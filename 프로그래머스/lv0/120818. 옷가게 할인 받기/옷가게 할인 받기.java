f214fa4f4c7e2026b16f78007264dac8c7c870ba
Kim Yu kyeong <kyk940608@gmail.com>
2023-04-11 21:10:38 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -2,6 +2,7 @@ class Solution {
     public int solution(int price) {
         int answer = 0;
         
+        //큰 수부터 점점 작은 수로 내려가는 것이 포인트
         if (price >= 500000)
             answer = (int)(price * 0.8);
         else if (price >= 300000)
@@ -9,7 +10,7 @@ class Solution {
         else if (price >= 100000)
             answer = (int)(price * 0.95);
         else
-            answer = price;
+            answer = price; //가격이 100000만원 미만일 때 주의 
         return answer;
     }
 }
