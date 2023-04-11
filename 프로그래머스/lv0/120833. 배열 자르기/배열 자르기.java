0b38d5d42ee009c84757346a63bb453578a5fd8b
Kim Yu kyeong <kyk940608@gmail.com>
2023-04-11 21:42:25 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -3,5 +3,6 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         return Arrays.copyOfRange(numbers, num1, num2+1);
+        //범위가 from ~ to이기 때문에 +1을 해줘야 num2값이 들어감
     }
 }
